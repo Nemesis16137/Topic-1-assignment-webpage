953163826b64ab38618d2dda728801d9f1d77d33
Nemesis16137 <jharrison2@edisonohio.edu>
2019-09-16 17:26:14 -0400

    4 files modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Import stylesheets
 import './style.css';
 
+$('input').on('change', function () {
+    var v = $(this).val();
+    $('div').css('font-size', v + 'px')
+    $('span').html(v);
+});
