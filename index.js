e4cdf834bd4a68f5a831829d3d043d671b0fff61
Nemesis16137 <jharrison2@edisonohio.edu>
2019-09-16 17:46:37 -0400

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Import stylesheets
 import './style.css';
-import * as $ from 'jquery';
+import $ from 'jquery';
+
+$('input').on('change', function () {
+    var v = $(this).val();
+    $('div').css('font-size', v + 'px')
+    $('span').html(v);
+});
 
 $('input').on('change', function () {
     var v = $(this).val();
